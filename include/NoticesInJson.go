1d160bdd0fae81ced19a82572f6dffc1841cea95
kryhin <artem@maxtvmedia.com>
2020-06-21 18:22:33 +0300

    I have added one mote test. Check if playlist has active notices. If not, skip it.

--- include/NoticesInJson.go
+++ include/NoticesInJson.go
@@ -66,52 +66,62 @@ func DoNoticesInJsonTest(run_type string) {
 				log.Println("Found in DB " + strconv.Itoa(len(DBNotices)) + " notices")
 			}
 
-			ServerNotices := GetServerPlaylistJson(playlist.Id)
-			if ServerNotices == nil {
-				var NoticeError TestError
-				NoticeError.TestId = test.ID
-				NoticeError.Type = "GetNoticesFromServerError"
-				NoticeError.Message = "Playlist :'" + playlist.Title + "' (" + strconv.Itoa(playlist.Id) + ") error getting json From Server"
-				Db.Create(&NoticeError)
-				test.ErrorCount += 1
-				continue
-			} else {
-				log.Println("Found on server " + strconv.Itoa(len(ServerNotices)) + " notices")
-			}
-
-			var NoticeFound bool
-			for _, DBNotice := range DBNotices {
-				if DBNotice.Status == "active" {
-					NoticeFound = false
-					log.Println("==================================================")
-					log.Println(DBNotice.Title)
-					log.Println("Start :", DBNotice.Schedule.ActivateFrom)
-					log.Println("End :", DBNotice.Schedule.ActivateTo)
-					for _, ServerNotice := range ServerNotices {
-						if ServerNotice.PageId == DBNotice.Id {
-							NoticeFound = true
-							break
-						}
-					}
-
-					if !NoticeFound {
-						log.Println("Notice '" + DBNotice.Title + "' not found in JSON")
-
-						linktonotice := os.Getenv("PMI_NOTICE_URL") + "#/notices/edit/" + strconv.Itoa(DBNotice.Id) + "/message"
-
-						var NoticeError TestError
-						NoticeError.TestId = test.ID
-						NoticeError.Type = "NoticeJSONError"
-
-						NoticeError.Message = "Playlist :'" + playlist.Title + "' (" + strconv.Itoa(playlist.Id) + "), Notice ID : " + strconv.Itoa(DBNotice.Id) +
-							" is not found in JSON, link to notice <a href=\"" + strconv.Itoa(DBNotice.Id) + "\">" + linktonotice + "</a>"
-
-						Db.Create(&NoticeError)
-						test.ErrorCount += 1
-					}
+			var activeIsHere bool
+			for _, notice := range DBNotices {
+				if notice.Status == "active" {
+					activeIsHere = true
+					break
 				}
 			}
-			test.PlayListsTested++
+
+			if activeIsHere {
+				ServerNotices := GetServerPlaylistJson(playlist.Id)
+				if ServerNotices == nil {
+					var NoticeError TestError
+					NoticeError.TestId = test.ID
+					NoticeError.Type = "GetNoticesFromServerError"
+					NoticeError.Message = "Playlist :'" + playlist.Title + "' (" + strconv.Itoa(playlist.Id) + ") error getting json From Server"
+					Db.Create(&NoticeError)
+					test.ErrorCount += 1
+					continue
+				} else {
+					log.Println("Found on server " + strconv.Itoa(len(ServerNotices)) + " notices")
+				}
+
+				var NoticeFound bool
+				for _, DBNotice := range DBNotices {
+					if DBNotice.Status == "active" {
+						NoticeFound = false
+						log.Println("==================================================")
+						log.Println(DBNotice.Title)
+						log.Println("Start :", DBNotice.Schedule.ActivateFrom)
+						log.Println("End :", DBNotice.Schedule.ActivateTo)
+						for _, ServerNotice := range ServerNotices {
+							if ServerNotice.PageId == DBNotice.Id {
+								NoticeFound = true
+								break
+							}
+						}
+
+						if !NoticeFound {
+							log.Println("Notice '" + DBNotice.Title + "' not found in JSON")
+
+							linktonotice := os.Getenv("PMI_NOTICE_URL") + "#/notices/edit/" + strconv.Itoa(DBNotice.Id) + "/message"
+
+							var NoticeError TestError
+							NoticeError.TestId = test.ID
+							NoticeError.Type = "NoticeJSONError"
+
+							NoticeError.Message = "Playlist :'" + playlist.Title + "' (" + strconv.Itoa(playlist.Id) + "), Notice ID : " + strconv.Itoa(DBNotice.Id) +
+								" is not found in JSON, link to notice <a href=\"" + strconv.Itoa(DBNotice.Id) + "\">" + linktonotice + "</a>"
+
+							Db.Create(&NoticeError)
+							test.ErrorCount += 1
+						}
+					}
+				}
+				test.PlayListsTested++
+			}
 		}
 		test.Status = "Completed"
 	}
